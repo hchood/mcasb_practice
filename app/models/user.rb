9742e189dd9549df7e617d86ca7e58667b3728fe
Helen Hood <helen.c.hood@gmail.com>
2014-08-19 16:43:27 -0400

    Add methods to create new user & new session on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,21 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :username, uniqueness: true
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
 end
