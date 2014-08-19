82f1456ae5a8049134db557fb9371d33c80ae204
Helen Hood <helen.c.hood@gmail.com>
2014-08-19 16:42:53 -0400

    Remove uniqueness constraint on username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :username, presence: true, uniqueness: true
+  validates :username, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140819175030) do
+ActiveRecord::Schema.define(version: 20140819192903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20140819175030) do
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "username",                            null: false
+    t.string   "username"
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
     t.string   "reset_password_token"
