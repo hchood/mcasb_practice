7be0ead90b51cca84a24d40edc7e03c9aff1b5c2
Helen Hood <helen.c.hood@gmail.com>
2014-08-21 13:12:37 -0400

    Change description to text field

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140821154404) do
+ActiveRecord::Schema.define(version: 20140821165428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "exercises", force: true do |t|
     t.string "name",        null: false
-    t.string "description", null: false
+    t.text   "description", null: false
   end
 
   add_index "exercises", ["name"], name: "index_exercises_on_name", unique: true, using: :btree
