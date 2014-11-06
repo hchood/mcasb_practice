f4c4f890ce4aecff0a85ae5a988d64e99fd69a81
Helen Hood <helen.c.hood@gmail.com>
2014-11-06 12:11:20 -0500

    User views exercise spec passing

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :exercises, only: [:new, :create]
   end
 
-  resources :exercises, only: :index
+  resources :exercises, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
