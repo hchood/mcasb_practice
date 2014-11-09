dc4d1329426f9230f392e6ece958a08f6f47bab3
Helen Hood <helen.c.hood@gmail.com>
2014-11-09 16:19:42 -0500

    Accept nested attributes for workout_exercises

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,6 +3,8 @@ class Workout < ActiveRecord::Base
     presence: true,
     uniqueness: true
 
-  has_many :workout_exercises
+  has_many :workout_exercises, dependent: :destroy
   has_many :exercises, through: :workout_exercises
+
+  accepts_nested_attributes_for :workout_exercises, reject_if: :all_blank, allow_destroy: true
 end
