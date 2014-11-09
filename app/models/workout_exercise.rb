ea71d9ce68995734fa82a82ed7b7f17ae8155e82
Helen Hood <helen.c.hood@gmail.com>
2014-11-09 16:25:48 -0500

    Remove workout validation on join table
    Join records wouldn’t persist since workout wasn’t created when they
    were being saved

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -2,7 +2,6 @@ class WorkoutExercise < ActiveRecord::Base
   belongs_to :workout
   belongs_to :exercise
 
-  validates :workout, presence: true
   validates :exercise, presence: true
   validates :exercise_number,
     presence: true,
